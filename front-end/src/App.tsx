e1f323ebddb955fc16b1f5b6295408367c1e4646
Om-Midya <dukemidya7521@gmail.com>
2024-06-13 22:57:41 +0530

    Recreated UI concept with tailwind CSS

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,13 +4,25 @@ function App() {
 
   return (
     <>
-      <div className="container">
-          <h1 className={"heading"}>OK Noted!</h1>
+      <div className="max-w-3xl mx-auto bg-white shadow-md rounded p-5 my-5 space-y-4">
+          <h1 className={"font-semi-bold text-5xl text-center"}>OK Noted!</h1>
           <div>
-              <input type={"text"} placeholder={"Title"} className={"input"}/>
+              <input type={"text"} placeholder={"Title"} className={"w-full border-b-2 border-gray-700 outline-none"}/>
           </div>
           <div>
-              <textarea placeholder={"Content"} className={"content"}/>
+              <textarea placeholder={"Content"} className={"w-full border-b-2 border-gray-700 outline-none resize-none h-20"}/>
+          </div>
+          <div className={"text-right"}>
+              <button className={"p-2 bg-blue-400 rounded-[5px]"}>Submit</button>
+          </div>
+
+          <div className={"max-w-3xl mx-auto bg-white shadow-lg rounded p-5 my-5 space-y-4 border-2"}>
+              <h1>Note 1 Heading</h1>
+              <div className={"flex space-x-2"}>
+                  <button className={"bg-green-300 p-2 rounded-[5px]"}>View</button>
+                  <button className={"bg-gray-300 p-2 rounded-[5px]"}>Edit</button>
+                  <button className={"bg-red-300 p-2 rounded-[5px]"}>Delete</button>
+              </div>
           </div>
       </div>
     </>
