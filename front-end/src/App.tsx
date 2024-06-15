bca0f5a88964ab431f8ca539a8e848b7df8ea081
Om-Midya <dukemidya7521@gmail.com>
2024-06-15 16:07:29 +0530

    Updated form validation with better data management

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,36 +1,48 @@
 import './App.css'
 import NoteItem from "./components/NoteItem.tsx";
-import {useState} from "react";
+import {ChangeEventHandler, useState} from "react";
 
 function App() {
-    const [title, setTitle] = useState<string>('')
-    const [content, setContent] = useState<string>('')
+    const[values, setValues] = useState({
+        title: "",
+        content: ""
+    })
+
+    // little complex function to handle input changes, but better data handling
+    const handleChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = ({target}/*destructured e.target from e*/) =>{
+        setValues({
+            ...values,
+            [target.name]: target.value
+        })
+    }
+
     return (
         <>
           <div className="max-w-3xl mx-auto bg-white shadow-md rounded p-5 my-5 space-y-4">
               <h1 className={"font-semi-bold text-5xl text-center"}>OK Noted!</h1>
               <div>
-                  {title.replace(/\s/g, '').length < 3? <p className={"text-red-500"}>Title must be at least 3 characters</p> : null}
+                  {values.title.replace(/\s/g, '').length < 3? <p className={"text-red-500"}>Title must be at least 3 characters</p> : null}
                   <input
                       type={"text"}
                       placeholder={"Title"}
                       className={"w-full border-b-2 border-gray-700 outline-none"}
-                      value={title}
-                      onChange={(e) => setTitle(e.target.value)}
+                      value={values.title}
+                      name={"title"}
+                      onChange={handleChange}
                   />
               </div>
               <div>
                   <textarea
                       placeholder={"Content"}
                       className={"w-full border-b-2 border-gray-700 outline-none resize-none h-20"}
-                      value={content}
-                      onChange={(e) => setContent(e.target.value)}
+                      value={values.content}
+                      name={"content"}
+                      onChange={handleChange}
                   />
               </div>
               <div className={"text-right"}>
                   <button className={"p-2 bg-blue-400 rounded-[5px]"} onClick={()=>{
-                        console.log("Title: ", title)
-                        console.log("Content: ", content)
+                        console.log(values)
                   }}>Submit</button>
               </div>
 
